SlimRoms/packages_apps_Settings

Settings: ADB_NOTIFY moved to Global
Change-Id: I6fc6d8ab8c7a4dd7c5f441b9dfa9166822051e82

src/com/android/settings/DevelopmentSettings.java
@@ -517,8 +517,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mHaveDebugSettings = false;
         updateSwitchPref(mEnableAdb, Settings.Global.getInt(cr,
                 Settings.Global.ADB_ENABLED, 0) != 0);
-        updateSwitchPref(mAdbNotify, Settings.Secure.getInt(cr,
-                Settings.Secure.ADB_NOTIFY, 1) != 0);
+        updateSwitchPref(mAdbNotify, Settings.Global.getInt(cr,
+                Settings.Global.ADB_NOTIFY, 1) != 0);
         if (mEnableTerminal != null) {
             updateSwitchPref(mEnableTerminal,
                     context.getPackageManager().getApplicationEnabledSetting(TERMINAL_APP_PACKAGE)
@@ -1394,8 +1394,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                         .setNegativeButton(android.R.string.cancel, null)
                         .show();
         } else if (preference == mAdbNotify) {
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.ADB_NOTIFY,
+            Settings.Global.putInt(getActivity().getContentResolver(),
+                    Settings.Global.ADB_NOTIFY,
                     mAdbNotify.isChecked() ? 1 : 0);
         } else if (preference == mEnableTerminal) {
             final PackageManager pm = getActivity().getPackageManager();
@@ -1560,8 +1560,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                 mDialogClicked = true;
                 Settings.Global.putInt(getActivity().getContentResolver(),
                         Settings.Global.ADB_ENABLED, 1);
-                Settings.Secure.putInt(getActivity().getContentResolver(),
-                        Settings.Secure.ADB_NOTIFY, 1);
+                Settings.Global.putInt(getActivity().getContentResolver(),
+                        Settings.Global.ADB_NOTIFY, 1);
                 mAdbNotify.setChecked(true);
                 mVerifyAppsOverUsb.setEnabled(true);
                 updateVerifyAppsOverUsbOptions();

RadonX-ROM/android_packages_apps_Settings

Change-Id: Idf0b3c88b55bced2a9ee4bd66876ac72331ed0dc

src/com/android/settings/DevelopmentSettings.java
@@ -509,8 +509,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mHaveDebugSettings = false;
         updateSwitchPreference(mEnableAdb, Settings.Global.getInt(cr,
                 Settings.Global.ADB_ENABLED, 0) != 0);
-        updateSwitchPreference(mAdbNotify, Settings.Secure.getInt(cr,
-                Settings.Secure.ADB_NOTIFY, 1) != 0);
+        updateSwitchPreference(mAdbNotify, Settings.Global.getInt(cr,
+                Settings.Global.ADB_NOTIFY, 1) != 0);
         if (mEnableTerminal != null) {
             updateSwitchPreference(mEnableTerminal,
                     context.getPackageManager().getApplicationEnabledSetting(TERMINAL_APP_PACKAGE)
@@ -1389,8 +1389,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                         .setNegativeButton(android.R.string.cancel, null)
                         .show();
         } else if (preference == mAdbNotify) {
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.ADB_NOTIFY,
+            Settings.Global.putInt(getActivity().getContentResolver(),
+                    Settings.Global.ADB_NOTIFY,
                     mAdbNotify.isChecked() ? 1 : 0);
         } else if (preference == mEnableTerminal) {
             final PackageManager pm = getActivity().getPackageManager();
@@ -1550,8 +1550,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                 mDialogClicked = true;
                 Settings.Global.putInt(getActivity().getContentResolver(),
                         Settings.Global.ADB_ENABLED, 1);
-                Settings.Secure.putInt(getActivity().getContentResolver(),
-                        Settings.Secure.ADB_NOTIFY, 1);
+                Settings.Global.putInt(getActivity().getContentResolver(),
+                        Settings.Global.ADB_NOTIFY, 1);
                 mAdbNotify.setChecked(true);
                 mVerifyAppsOverUsb.setEnabled(true);
                 updateVerifyAppsOverUsbOptions();